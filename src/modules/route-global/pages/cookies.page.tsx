9d7f70255d423616b4558c7fb7ff9591435def35
d4search <139180262+d4search@users.noreply.github.com>
2023-08-27 15:21:03 -0400

    Fix/fix i18n json (#60)
    * Update cookies.page.tsx (#45)
    
    * Update cookies.page.tsx
    
    Adding in basic cookies page
    
    * Update cookies.page.tsx
    
    * Update cookies.page.tsx
    
    * Fix i18n files
    
    ---------
    
    Co-authored-by: Nick <96056869+softwarecomplexity@users.noreply.github.com>
    Co-authored-by: Devoder <devoder@d4search.com>

--- src/modules/route-global/pages/cookies.page.tsx
+++ src/modules/route-global/pages/cookies.page.tsx
@@ -1,10 +1,22 @@
 import { styled } from '@mui/material/styles';
+import ReactMarkdown from 'react-markdown';
+import content from './cookies.page.md?raw';
 
-const Content = styled('div')(() => ({}));
+const Content = styled('div')(({ theme }) => ({
+    '*': {
+        userSelect: 'text',
+    },
+    'a': {
+        color: theme.palette.primary.main,
+    },
+}));
 
 export const CookiesPage: React.FC = () => {
     return (
         <Content>
+            <ReactMarkdown>
+                {content}
+            </ReactMarkdown>
         </Content>
     );
 };
