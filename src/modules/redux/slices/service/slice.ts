43470b3eb669150c27f35670f2d9fb8212ee3fe6
Dennis Long <dennis@digitalflavors.net>
2023-08-30 20:11:59 -0700

    ran yarn build and format

--- src/modules/redux/slices/service/slice.ts
+++ src/modules/redux/slices/service/slice.ts
@@ -76,6 +76,6 @@ export const ServiceSlice = createSlice({
                         return updatedResult ? updatedResult : listing;
                     });
                 },
-            )
+            );
     },
 });
